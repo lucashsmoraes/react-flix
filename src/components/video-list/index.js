13b12402334a7e60d7871bca17a880f18b95a68f
lucashsmoraes <lucashsmoraes@gmail.com>
2019-10-20 20:08:23 -0300

    separando o componente VideoList e seus estilos

---
+++ src/components/video-list/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import styled from 'styled-components'
+import Play from '../play'
+
+const VideosList = () => (
+  <Container>
+    {Array.from({length: 10}).map((item, index) => (
+      <Video key={index}>
+        <VideoThumb >
+          <PlayStyled />
+        </VideoThumb>
+        <VideoTitle>Título do Vídeo</VideoTitle>
+      </Video>
+    ))}
+  </Container>
+)
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+const Video = styled.section`
+  flex: 1 1 300px;
+  margin: 0 5px 5px;
+`
+const VideoThumb = styled.div`
+  align-items: center;
+  border: 1px solid #999;
+  display: flex;
+  height: 150px;
+  justify-content: center;
+`
+const PlayStyled = styled(Play)`
+  fill: #999;
+  height: 50px;
+  width: 50px;
+`
+const VideoTitle = styled.h2`
+  font-size: 18px;
+`
+
+export default VideosList
